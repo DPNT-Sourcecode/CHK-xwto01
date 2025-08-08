2ff0fadbccf48b699ea41900b2992bd4593c63a7
sbx_user1051 <sbx_user1051@169.254.11.13>
2025-08-08 11:04:48 +0000

    Fri Aug 08 11:04:48 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -2,18 +2,34 @@ package io.accelerate.solutions.CHK;
 
 import io.accelerate.runner.SolutionNotImplementedException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        char[] str = skus.toCharArray() , hash = new char[26];
-        int total = 0;
+        char[] str = skus.toCharArray() , hash = new char[4];
+        int total = 0 ,index;
+        int[] values = new int[]{50,30,20,15};
+        int[][] deals = new int[][]{{3,20}, {2,15} , {Integer.MAX_VALUE,0}, {Integer.MAX_VALUE,0}};
 
         for(char x : str){
-            hash[ x - 'A']++;
+            index = x - 'A';
+            if(index > 3 || index < 0 ){
+                return -1;
+            }
+            hash[index]++;
+
+            total += values[index];
+
+            if(hash[index] % deals[index][0] == 0 ){
+                total -= deals[index][1];
+            }
 
         }
 
 
-
+        return total;
     }
 }
+
 
